b883291f7e5d9a991cb5660c0cc4240386546011
arghpy <subaandrei2@gmail.com>
2026-02-04 07:23:33 +0200

    refactor: move valid_move, is_legal_move and check stuff in general

--- include/rules/general.h
+++ include/rules/general.h
@@ -4,6 +4,11 @@
 #include <stdbool.h>
 #include "init.h"
 
+bool is_legal_move(ChessSquare *src, ChessSquare *dest, ChessPieceType type);
+bool valid_move(ChessSquare *src, ChessSquare *dest);
+bool verify_check(ChessPieceColor verify_color);
+void gave_check(void);
+bool verify_checkmate(ChessPieceColor verify_color);
 bool still_on_src_square(const ChessSquare *src, const ChessSquare *dest);
 bool capture_king(const ChessSquare *dest);
 bool capture_ally(const ChessSquare *dest);
